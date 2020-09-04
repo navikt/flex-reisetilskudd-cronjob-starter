2a2df59e4083c3220aabf3023b4e515bb0ef3c4a
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2020-09-04 13:43:36 +0200

    lint

--- src/main/kotlin/no/nav/helse/flex/Bootstrap.kt
+++ src/main/kotlin/no/nav/helse/flex/Bootstrap.kt
@@ -3,10 +3,8 @@ package no.nav.helse.flex
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-
 val log: Logger = LoggerFactory.getLogger("no.nav.helse.flex.flex-reisetilskudd-cronjob-starter")
 
 fun main() {
     log.info("Starter flex-reisetilskudd-cronjob-starter")
 }
-
